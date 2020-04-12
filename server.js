7b557651316488cba75be75ea6bb7ba93693e2d9
sodiq oyedotun <oyedotunsodiq045@gmail.com>
2020-04-12 12:03:45 +0100

    Initial commit, dir and files

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+
+// Route files
+const courses = require('./routes/executives');
+
+// Load env vars
+dotenv.config({
+  path: './config/config.env'
+});
+
+// Connect DB
+connectDB();
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/executives', executives);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5004;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow
+      .bold
+  )
+);
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
